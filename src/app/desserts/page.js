fee7b1e29ae01d6b881d4064d0a8cceeab56332a
Amuro Brian <perezbrian.sti@gmail.com>
2025-03-02 00:09:55 +0800

    Added pages and templates for developers

---
+++ src/app/desserts/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Desserts() {
+    return (
+        <div>
+            Desserts Page
+        </div>
+    )
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import { Utensils, CupSoda, IceCream, ShoppingCart } from "lucide-react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,17 +24,21 @@ export default function RootLayout({ children }) {
       <body>
         <div className="flex h-screen">
           {/* Sidebar */}
-          <aside className="w-64 bg-gray-900 text-white p-5 flex flex-col space-y-4">
+          <aside className="w-64 bg-[var(--lightyellow)] text-white p-5 flex flex-col space-y-4">
             <div><img src="/images/logo.png" alt="MesaMate Logo" className="w-full" /></div>
-            <nav className="flex flex-col space-y-2">
-              <Link href="/dashboard" className="hover:bg-gray-700 p-2 rounded">
-                Dashboard
+            <div className="w-full h-[50px]"></div>
+            <nav className="flex flex-col space-y-2 gap-4">
+              <Link href="/foods" className="hover:bg-[var(--yellowbg)] p-2 rounded text-black flex gap-2">
+                <Utensils size={20} /> FOODS
               </Link>
-              <Link href="/settings" className="hover:bg-gray-700 p-2 rounded">
-                Settings
+              <Link href="/drinks" className="hover:bg-[var(--yellowbg)] p-2 rounded text-black flex gap-2">
+                <CupSoda size={20} /> DRINKS
               </Link>
-              <Link href="/profile" className="hover:bg-gray-700 p-2 rounded">
-                Profile
+              <Link href="/desserts" className="hover:bg-[var(--yellowbg)] p-2 rounded text-black flex gap-2">
+                <IceCream size={20} /> DESSERTS
+              </Link>
+              <Link href="/carts" className="hover:bg-[var(--yellowbg)] p-2 rounded text-black flex gap-2">
+                <ShoppingCart size={20} /> CARTS
               </Link>
             </nav>
           </aside>
